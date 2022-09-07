3d79dfc72a91d35d56b872f2ede25579dc7d5494
rathanaIO <rathana.krean@qwiq.io>
2022-09-07 09:57:53 +0700

    new feature

--- TyingBoot.py
+++ TyingBoot.py
@@ -1,15 +1,32 @@
+#######  install dependency ############
+# pip3 install webdriver-manager
+# pip3 install -U selenium
+#######################################
+
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from pynput.keyboard import Key, Listener
 from selenium.webdriver.common.by import By
 from pynput.keyboard import Key, Controller
 import time
 
 # # web driver
-browser = webdriver.Chrome()
-browser.get('https://play.typeracer.com?rt=4tpznkop1')# time.sleep(4)
-# browser.find_element(By.XPATH,'//*[@id="gwt-uid-1"]/a').click()
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_experimental_option("detach", True)
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-
+browser.get('https://play.typeracer.com?rt=2lobbruul0')
+time.sleep(4)
+# browser.find_element(By.XPATH, '//*[@id="gwt-uid-1"]/a').click()
+#
 def show(key):
     #press enter to start auto typing
     if key == Key.enter:
@@ -21,15 +38,14 @@ def show(key):
             return [char for char in words]
         for i in split_words(words):
             keyboard.type(i)
-            time.sleep(0.07)
-
-    # by pressing 'delete' button
-    # you can terminate the loop
+            # time.sleep(0.07) # 120wpm
+            # time.sleep(0.05) #188wpm
+            time.sleep(0.02) #370wpm
+    # by pressing 'delete' button to terminate the loop
     if key == Key.delete:
         return False
 
-
-# Collect all event until released
+    # Collect all event until released
 with Listener(on_press=show) as listener:
     listener.join()
 
